2a8f57997b821c95dbdaf9de377c1c58d8d2c22b
shalakaLondonNorwich <shalaka.javheri@gmail.com>
2026-07-10 20:20:27 +0530

    initial project structure

---
+++ src/test/java/com/shalaka/banksphere/BanksphereApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shalaka.banksphere;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BanksphereApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
